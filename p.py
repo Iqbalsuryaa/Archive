d64925fd355f68a9c2469e76142f0b3bd8c60d1b
Mohammad Iqbal Surya Ramadhan <112038977+Iqbalsuryaa@users.noreply.github.com>
2023-06-13 22:22:57 +0700

    Update p.py

--- p.py
+++ p.py
@@ -17,10 +17,10 @@ if (selected2 == 'Data') :
     st.title('deskripsi data')
 
     st.write("Ini adalah contoh data yang tersedia dalam aplikasi Streamlit.")
-    st.write("Data ini berisi informasi tentang sebuah bank (Bank Thera) yang manajemennya ingin mencari cara untuk mengubah pelanggan pertanggungjawabannya menjadi pelanggan pinjaman pribadi (sambil mempertahankan mereka sebagai deposan")
+    st.write("Data ini berisikan informasi mengenai pria dan wanita, yang bertujuan untuk mengetahui memiliki rambut yang panjang atau tidak.")
     st.write("Data ini diambil dari kaggle")
     st.write("Data ini merupakan type data Numerik")
-    data = pd.read_csv('Bank_Personal_Loan_Modelling.csv')
+    data = pd.read_csv('gender_classification_v7_oke.csv')
     st.write(data)
 
 
@@ -34,20 +34,21 @@ if (selected2 == 'Procecing data') :
     data = pd.read_csv('preprocessed_data.csv')
     st.write(data)
 
+
 #halaman modelling
 if (selected2 == 'Modelling'):
     st.title('Modelling')
 
-    pilih = st.radio('Pilih', ('Naive Bayes', 'Decision Tree', 'KNN', 'ANN'))
+    pilih = st.radio('Pilih', ('Naive Bayes', 'Decision Tree', 'KNN', 'MLP'))
 
     if (pilih == 'Naive Bayes'):
-        st.title(' Nilai Akurasi 70,8%')
+        st.title(' Nilai Akurasi 100%')
     elif (pilih == 'Decision Tree'):
-        st.title(' Nilai Akurasi 75%')
+        st.title(' Nilai Akurasi 100%')
     elif (pilih == 'KNN'):
-        st.title(' Nilai Akurasi 65%')
-    elif (pilih == 'ANN'):
-        st.title(' Nilai Akurasi 73,8%')
+        st.title(' Nilai Akurasi 100%')
+    elif (pilih == 'MLP'):
+        st.title(' Nilai Akurasi 100%')
 
 
 #halaman implementasi
@@ -55,74 +56,61 @@ if (selected2 == 'Modelling'):
 if (selected2 == 'Implementasi'):
     st.title('Implementasi')
 
-
     model_filename = 'loan.pkl'
     with open(model_filename, 'rb') as file:
         model = pickle.load(file)
 
     # Create a function to preprocess the input data
-    def preprocess_input(data):
-        # Convert the input data into a DataFrame
-        df = pd.DataFrame(data, index=[0])
-        
-        # Standarisasi fitur
-        scaler = StandardScaler()
-        scaler.fit(df)  # Fit the scaler on the input data
-        df_std = scaler.transform(df)
-        
-        return df_std
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
 
-    # Create the Streamlit web app
-    def main():
-        st.title('Personal Loan Bank')
+# Memuat data yang telah diproses sebelumnya
+df_std = pd.read_csv('preprocessed_data.csv')
 
-        # Create input fields for the features
-        id = st.text_input('ID')
-        age = st.number_input('Umur', min_value=0, max_value=100, value=30)
-        experience = st.number_input('Pengalaman', min_value=0, max_value=100, value=10)
-        income = st.number_input('Pendapatan', min_value=0, value=50000)
-        zip_code = st.number_input('Kode Pos', min_value=0, value=10000)
-        family = st.number_input('Keluarga', min_value=0, value=1)
-        cc_avg = st.number_input('CCAvg', min_value=0, value=1)
-        education = st.number_input('Pendidikan', min_value=1, max_value=3, value=1)
-        mortgage = st.number_input('Mortgage', min_value=0, value=0)
-        personal_loan = st.number_input('Pinjaman Personal', min_value=0, max_value=1, value=0)
-        securities_account = st.selectbox('Rekening Efek', [0, 1])
-        cd_account = st.selectbox('Rekening CD', [0, 1])
-        online = st.selectbox('Online', [0, 1])
+# Menyiapkan data untuk pemodelan
+X = df_std[['gender', 'forehead_width', 'forehead_height_cm', 'nose_long', 'lips_thin', 'distance_nose_to_lip_long']].values
+y = df_std['long_hair'].values
+
+# Menstandarisasi fitur-fitur
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+# Melatih model klasifikasi Gaussian Naive Bayes
+classifier = GaussianNB()
+classifier.fit(X, y)
+
+# Fungsi untuk memprediksi rambut panjang berdasarkan input pengguna
+def predict_long_hair(gender, forehead_width, forehead_height, nose_long, lips_thin, distance_nose_to_lip_long):
+    input_data = [gender, forehead_width, forehead_height, nose_long, lips_thin, distance_nose_to_lip_long]
+    input_data = sc.transform([input_data])
+    predicted_long_hair = classifier.predict(input_data)
+    return predicted_long_hair[0]
+
+# Aplikasi web menggunakan Streamlit
+def main():
+    st.title("Prediksi Panjang Rambut")
+    st.write("Masukkan informasi berikut untuk memprediksi apakah seseorang memiliki rambut panjang atau tidak:")
+
+    gender = st.selectbox("Jenis Kelamin", ["Perempuan", "Laki-laki"])
+    forehead_width = st.slider("Lebar Dahi", min_value=0.0, max_value=1.0, step=0.01)
+    forehead_height = st.slider("Tinggi Dahi (cm)", min_value=0.0, max_value=1.0, step=0.01)
+    nose_long = st.slider("Panjang Hidung", min_value=0.0, max_value=1.0, step=0.01)
+    lips_thin = st.slider("Ketipisan Bibir", min_value=0.0, max_value=1.0, step=0.01)
+    distance_nose_to_lip_long = st.slider("Jarak dari Hidung ke Bibir", min_value=0.0, max_value=1.0, step=0.01)
+
+    if st.button("Prediksi"):
+        predicted_hair_length = predict_long_hair(gender, forehead_width, forehead_height, nose_long, lips_thin, distance_nose_to_lip_long)
+        if predicted_hair_length == 1:
+            st.write("Orang tersebut diprediksi memiliki rambut panjang.")
+        else:
+            st.write("Orang tersebut diprediksi tidak memiliki rambut panjang.")
+
+if __name__ == '__main__':
+    main()
 
 
-        # Create a dictionary with the input data
-        input_data = {
-            'ID': id,
-            'Age': age,
-            'Experience': experience,
-            'Income': income,
-            'ZIP Code': zip_code,
-            'Family': family,
-            'CCAvg': cc_avg,
-            'Education': education,
-            'Mortgage': mortgage,
-            'Personal Loan': personal_loan,
-            'Securities Account': securities_account,
-            'CD Account': cd_account,
-            'Online': online,
-            
-        }
 
-        # Perform prediction when the button is pressed
-        if st.button('Hitung'):
-            # Preprocess the input data
-            input_data_std = preprocess_input(input_data)
-            
-            # Predict using the loaded model
-            prediction = model.predict(input_data_std)
-
-            # Display the prediction
-            if prediction[0] == 0:
-                st.error('Tidak Memenuhi Syarat untuk Pinjaman Personal')
-            else:
-                st.success('Memenuhi Syarat untuk Pinjaman Personal')
-
-    if _name_ == '_main_':
-        main()
+    
